24864b68ed898b12466ce1d8e96248545594443f
Erin Jeter <ekjeter@gmail.com>
2021-03-06 15:20:38 -0500

    add ssl language

--- db.js
+++ db.js
@@ -5,6 +5,12 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 //verify to test if connection is ok
