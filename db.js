aaeaacd829b86d8d492981544fe6846d33bb5b50
Erin Jeter <ekjeter@gmail.com>
2021-03-07 18:38:29 -0500

    updating main, local testin host

--- db.js
+++ db.js
@@ -4,6 +4,7 @@
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  // host: "localhost",
   dialect: "postgres",
   dialectOptions: {
     ssl: {
